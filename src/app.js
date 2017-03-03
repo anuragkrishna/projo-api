1eee2d3bae46ed4af24a6cac939d0fccf63700ee
anuragkrishna <anurag.bits18@gmail.com>
2017-03-03 20:37:09 +0530

    db connection code to bin/www

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@
 
 import express from 'express';
 import path from 'path';
-import favicon from 'serve-favicon';
 import cookieParser from  'cookie-parser';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 
 import index from './routes/index';
 import users from './routes/users';
@@ -28,10 +26,4 @@ app.use(function(req, res, next) {
 app.use('/', index);
 app.use('/api', p_route);
 
-//DB Connection
-var mongoDBUrl= 'mongodb://anurag:projo@ds145289.mlab.com:45289/projodb';
-mongoose.connect(mongoDBUrl);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-module.exports = app;
+export default app;
